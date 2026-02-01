024ae29ab8ea65cc27ecbbb3eeb1100748f9d8e0
elias <elias.stelzer2004@gmail.com>
2026-02-01 21:33:40 +0100

    fix build for render

---
+++ dist/lan-server.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const sqlite3_1 = __importDefault(require("sqlite3"));
+const db = new sqlite3_1.default.Database("sessions.db");
+const wss = new ws_1.WebSocketServer({ port: 6666 });
+wss.on("connection", (ws) => {
+    console.log("LAN Client verbunden, du oaschloch");
+    ws.on("message", (msg) => {
+        console.log("LAN Eingang:", msg.toString(), "du oaschloch");
+        // Antwort emulieren
+        if (msg.toString() === "create")
+            ws.send("session_created:local", "du oaschloch");
+        if (msg.toString() === "join")
+            ws.send("session_joined:local", "du oaschloch");
+        if (msg.toString() === "sync")
+            ws.send("sync_ok", "du oaschloch");
+        // Broadcast an alle Clients
+        wss.clients.forEach(c => {
+            if (c.readyState === 1)
+                c.send("player_update:" + msg.toString());
+        });
+    });
+});
+console.log("LAN Server läuft auf Port 6666, du oaschloch");
